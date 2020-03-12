f3922b5ed638b52a7dec30775c528cb62693d119
Sam Bellenchia <bellenchia@gmail.com>
2020-03-12 18:25:27 -0400

    .

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-My take on an NLP project, added verbosity and coded for error raised when get requests exceed servers liking
 
-UPDATE 03/2020: revised code to be more flexible and eliminate need for aggregation down the line
+UPDATE 03/2020: Added verbosity, made tool more flexible, and now writes preprocessed data to the .csv file.  
--- scraping_tool.py
+++ scraping_tool.py
@@ -6,22 +6,24 @@ Last Revised: 03/12/20
 -- and analyzes the sentiment intensity of the containing text, then writes that data to a 
 -- specified .csv file  
 
-'''
+NOTE: !pip install vaderSentiment to run this code
 
+'''
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 analyzer = SentimentIntensityAnalyzer()
 from datetime import datetime
 import numpy as np
 import requests
+import time
 import csv
 
 FILENAME       =   "_sentiment.csv"     # Designate filename to write data into
-CONTENT_TYPE   =   "comment"            # Choose "submission" or "comment"
+CONTENT_TYPE   =   "submission"         # Choose "submission" or "comment"
 KEYWORD        =   "tesla"              # Choose keyword to filter text by
 DIRECTORY      =   "./"                 # Set directory to write file into
 RANGE          =   365                  # How many days to extend the query
 SUBREDDITS     =   []                   # Leave empty to search all subreddits
-
+SLEEP_TIME     =   1                    # Time to pause between requests
 
 # Lambda function to contain long if/else construct
 date_format = lambda a: "{}-0{}-0{}".format(a.year, a.month, a.day) if( a.month < 10 and a.day < 10) else "{}-0{}-{}".format(a.year, a.month, a.day) if( a.month < 10 and a.day >= 10) else "{}-{}-0{}".format(a.year, a.month, a.day) if( a.month >= 10 and a.day < 10) else "{}-{}-{}".format(a.year, a.month, a.day)
@@ -38,39 +40,59 @@ def aggregate(documents, content, subreddit):
     total = np.zeros(4)    
     attribute = 'body' if content == 'comment' else 'selftext'
     for doc in documents:
-        text, utc = doc[attribute], doc['created_utc']
-        sent = analyzer.polarity_scores(text)     
-        formatted_date = format_date(utc) 
-        total += np.array([sent['pos'],sent['neg'],sent['neu'],sent['compound']])
+        try:
+            if doc['selftext'] == "":
+                frequency -= 1
+            else:
+                text, utc = doc[attribute], doc['created_utc']
+                sent = analyzer.polarity_scores(text)     
+                formatted_date = format_date(utc) 
+                total += np.array([sent['pos'],sent['neg'],sent['neu'],sent['compound']])
+        except KeyError:
+            print("KeyError")
+            frequency -= 1
 
     data = [formatted_date, frequency] + [val for val in total]
     
-    if sub:
+    if subreddit:
         data += [subreddit]
     
     return data
 
+if __name__ == "__main__":
 
-f = open(DIRECTORY+KEYWORD+FILENAME, "wt")
-writer = csv.writer(f)
-for day in range(1,RANGE):
-    if SUBREDDITS == []:
-        
-        # Format the pushshift url according to the query 
-        base = '{}/?q={}&after={}d&before={}d'.format(CONTENT_TYPE, KEYWORD, RANGE - day, RANGE - day - 1)        
-        URL = 'https://api.pushshift.io/reddit/search/{}&sort_type=score&sort=desc&size=1000&fields=body,created_utc'.format(base)
-        docs = requests.get(URL).json()['data']
-        
-        # Write data to file
-        row = aggregate(documents, content = CONTENT_TYPE, sub=None)
-        writer.writerow(row)
-
-    else:  
-        for SUB in SUBREDDITS:
-            base = '{}/?q={}&subreddit={}&after={}d&before={}d'.format(CONTENT_TYPE, KEYWORD, SUB, RANGE - day, RANGE - day - 1)        
-            URL = 'https://api.pushshift.io/reddit/search/{}&sort_type=score&sort=desc&size=1000&fields=body,created_utc'.format(base)
+    print("\nCurrent settings:\n\n",\
+        '''
+        FILENAME       =   "_sentiment.csv"     # Designate filename to write data into
+        CONTENT_TYPE   =   "submission"         # Choose "submission" or "comment"
+        KEYWORD        =   "tesla"              # Choose keyword to filter text by
+        DIRECTORY      =   "./"                 # Set directory to write file into
+        RANGE          =   365                  # How many days to extend the query
+        SUBREDDITS     =   []                   # Leave empty to search all subreddits
+        SLEEP_TIME     =   1                    # Time to pause between requests
+        ''',"\n\nSearching Reddit for {}s mentioning {} :\n".format(CONTENT_TYPE, KEYWORD))
+    f = open(DIRECTORY+KEYWORD+FILENAME, "wt")
+    writer = csv.writer(f)
+    attribute = 'body' if CONTENT_TYPE == 'comment' else 'selftext'
+    for day in range(1,RANGE):
+        time.sleep(SLEEP_TIME)
+        if SUBREDDITS == []:
+            
+            # Format the pushshift url according to the query 
+            base = '{}/?q={}&after={}d&before={}d'.format(CONTENT_TYPE, KEYWORD, RANGE - day, RANGE - day - 1)        
+            URL = 'https://api.pushshift.io/reddit/search/{}&sort_type=score&sort=desc&size=1000&fields=selftext,created_utc'.format(base)
             docs = requests.get(URL).json()['data']
-            row = aggregate(docs, content = CONTENT_TYPE, subreddit = sub)
+            
+            # Write data to file
+            row = aggregate(docs, CONTENT_TYPE, subreddit=None)
             writer.writerow(row)
 
-    print("Day {}/{}, {} {}s".format(day, RANGE, len(docs), CONTENT_TYPE))
+        else:  
+            for SUB in SUBREDDITS:
+                base = '{}/?q={}&subreddit={}&after={}d&before={}d'.format(CONTENT_TYPE, KEYWORD, SUB, RANGE - day, RANGE - day - 1)        
+                URL = 'https://api.pushshift.io/reddit/search/{}&sort_type=score&sort=desc&size=1000&fields=selftext,created_utc'.format(base)
+                docs = requests.get(URL).json()['data']
+                row = aggregate(docs, CONTENT_TYPE, SUB)
+                writer.writerow(row)
+
+        print("Day {}/{}, {} {}s".format(day, RANGE, len(docs), CONTENT_TYPE))
